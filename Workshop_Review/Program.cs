7ca7cff01db672d1b25acc3355aae3385da13da0
Komal <divyavairagade@gmail.com>
2022-05-18 10:00:48 +0530

    Add project files.

---
+++ Workshop_Review/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Workshop_Review;
+Console.WriteLine("Hello, World!");
+
+SnakeAndLadder obj = new SnakeAndLadder();
+Console.WriteLine(obj.DieRoll());
+obj.Game();
---
+++ Workshop_Review/SnakeAndLadder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop_Review
+{
+    public class SnakeAndLadder
+    {
+        int position = 0;
+        const int NO_PLAY = 0, SNAKE = 1, LADDER = 2;
+        public int DieRoll()
+        {
+            Random random = new Random();
+            int diePoistion = random.Next(1, 7);
+            return diePoistion;
+        }
+
+        public void Game()
+        {
+            Random rand = new Random();
+            int option = rand.Next(0, 3);
+            switch (option)
+            {
+                case 0:
+                    break;
+                case SNAKE:
+                    this.position -= this.DieRoll();
+                         break;
+                case LADDER:
+                    this.position += this.DieRoll();
+                        break;  
+            }
+            Console.WriteLine(this.position);
+
+        }
+    }
+}
